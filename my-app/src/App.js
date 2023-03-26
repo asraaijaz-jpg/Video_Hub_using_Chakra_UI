bf6bcdcb72ba3fb3fc9d46a13bcf28e49e0f719d
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-26 19:56:21 +0500

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router , Routes , Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+     <Routes>
+        <Route/>
+     </Routes>
+    </Router>
+     );
+}
+
+export default App;

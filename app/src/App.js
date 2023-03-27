66be3b9f11a009ec3ee8e46ea01ea73e01c75433
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-27 06:30:36 +0500

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { ChakraProvider,Box,Text,Link,VStack,Code,Grid,theme,} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {BrowserRouter as  Router , Routes , Route} from 'react-router-dom';
+import { Button } from '@chakra-ui/react';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-     
-    </ChakraProvider>
+  <Router>
+    <div>Hello Chakra_UI</div>
+    <Button colorScheme={'facebook'}>Click</Button>
+    <Button colorScheme={'whatsapp'}>Click</Button>
+  <Routes>
+    <Route/>
+  </Routes>
+  </Router>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,8 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript  , ChakraProvider , theme} from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -11,10 +10,12 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
+
+    {/*                  If we enclosed main app in chakraprovider then it automatically apply basic stylying         */}
+
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </StrictMode>
 );
 
-serviceWorker.unregister();
-
-reportWebVitals();

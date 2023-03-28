e8d4d43a534bd4e4acfa49eb95fb42b676e10fef
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-28 15:47:44 +0500

    commit

--- app/src/Component/Login.jsx
+++ app/src/Component/Login.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Container , VStack , Heading ,Input, Button} from '@chakra-ui/react';
+import {Container , Text ,VStack , Heading ,Input, Button} from '@chakra-ui/react';
 import {Link} from 'react-router-dom';
 
 const Login = () => {
     return (
-      <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'} >
 
 <form>
-<VStack alignItems={'stretch'} spacing={'8'} w={'96'} m={'auto'} my={'16'}>
+<VStack alignItems={'stretch'} spacing={'8'} w={['full' , '96']} m={'auto'} my={'16'}>
 <Heading>Welcome Back</Heading>
 
 <Input 
@@ -24,8 +24,20 @@ focusBorderColor={'purple.500'}/>
 
 <Button variant={'link'} alignSelf={'flex-end'}>
 <Link to={'/forgetpassword'}>Forget Password</Link>
-
 </Button>
+
+<Button type={'submit'} colorScheme={'purple'}>
+    Login
+</Button>
+
+<Text textAlign={'right'}>New User? { ' '}
+
+<Button variant={'link'} colorScheme={'purple'}>
+<Link to={'/signup'}>SignUp</Link>
+</Button>
+
+</Text>
+
 
 </VStack>
 </form>
